984bfade0cb2dfd5a7ebf86ad7c3769fd395f178
zeazn <146925887+zeazn@users.noreply.github.com>
2024-06-24 19:09:32 +1000

    Kaggle Notebook | My R Journey | Version 1

---
+++ my-r-journey.r
@@ -0,0 +1,98 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "781c7d5f",
+   "metadata": {
+    "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
+    "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
+    "papermill": {
+     "duration": 0.001739,
+     "end_time": "2024-06-24T09:09:22.705726",
+     "exception": false,
+     "start_time": "2024-06-24T09:09:22.703987",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "# My R journey - The New Beginning\n",
+    "\n",
+    "## Introduction\n",
+    "\n",
+    "Just recently, I started to learn R and just now have started a Kaggle notebook to document my journey.\n",
+    "\n",
+    "It has been an interesting few weeks and my brain still refuses to realise that this is not something I can learn overnight, or even in a couple of weeks. It will take time, and I need to allow time to practise (and not let my ADHD get the better of me).\n",
+    "\n",
+    "I never learned how to code, and short of building my Voron printer config file I have not had much exposure to anything like this. Sure, the 3D printer config file was not *reeeeeeeealy* coding, but there was formatting, there was commands and there was commenting, so I will take it as a win in my book!\n",
+    "\n",
+    "So, here we go. Good Luck to me, and hopefully someone finds this useful to them.\n",
+    "\n",
+    "## Down the rabbit hole we go.\n",
+    "\n",
+    "The first thing I realised with Kaggle, is that this defaults to python. And this should be the **first** thing you change before to write anything. Itis just good habit.\n",
+    "\n",
+    "It is easy enough to do:\n",
+    "\n",
+    "* Click File above\n",
+    "* Mouse over Language\n",
+    "* Select R\n",
+    "\n",
+    "Done. Easy right? Well that is just the start."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "6dc4bb79",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001081,
+     "end_time": "2024-06-24T09:09:22.708146",
+     "exception": false,
+     "start_time": "2024-06-24T09:09:22.707065",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30618,
+   "isGpuEnabled": false,
+   "isInternetEnabled": false,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.289591,
+   "end_time": "2024-06-24T09:09:22.829542",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2024-06-24T09:09:19.539951",
+   "version": "2.5.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
